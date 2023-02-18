f79dfba511a139e61319c766febfa28c1fa18a88
iwanttobe00 <xxhh0@mail.ru>
2023-02-18 18:24:19 +0400

    Update views

--- valuta/views.py
+++ valuta/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render
 import requests
-import re
 
 app_name = 'valuta'
 
 def currency(request):
-    response = requests.get(url='https://api.exchangerate-api.com/v4/latest/USD').json()
+    response = requests.get(url='https://api.exchangerate.host/latest').json()
     currencies = response.get('rates')
 
     if request.method == 'GET':
         context = {
-            'currencies': currencies
+            'currencies': currencies,
         }
         return render(request, 'index.html', context)
 
@@ -18,13 +17,12 @@ def currency(request):
         from_amount = float(request.POST.get('from_amount'))
         from_curr = request.POST.get('from_curr')
         to_curr = request.POST.get('to_curr')
-
         converted_amount = round((currencies[to_curr]) / currencies[from_curr] * float(from_amount), 2)
         context = {
             'from_curr': from_curr,
             'to_curr': to_curr,
             'from_amount': from_amount,
             'currencies': currencies,
-            'converted_amount': converted_amount
+            'converted_amount': converted_amount,
         }
         return render(request, 'index.html', context)
